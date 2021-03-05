502e1ac5de0e9f93b7d5e34b6ff40a9589e70f52
Noman-Tanveer <nomantanveer021@gmail.com>
2021-03-05 22:28:07 +0500

    edited file train.py

--- train.py
+++ train.py
@@ -1,1 +1,18 @@
 print('train.py Contaains the core training loop')
+
+import torch
+from torch import nn
+import torchvision
+import matplotlib
+import numpy
+
+class Net(Module):
+    def __init__(self):
+        nn.conv2d()
+
+for i in range(epochs):
+    # Training loop
+
+
+for j in range(len(val)):
+    # Validation loop
